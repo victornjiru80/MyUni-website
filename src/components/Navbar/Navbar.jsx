139cbce533e916284c0f93491f7a5b88247ad8a6
Victor <87243454+victornjiru80@users.noreply.github.com>
2025-04-19 18:03:13 +0300

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
 
 
   return (
-    <nav className={`container ${sticky? 'dark-nav' : ''}`}>
+    <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
         <img src={logo} className='logo' />
         <ul className={mobileMenu ? '': 'hide-mobile-menu'}>
           <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
